83e571e5f8752113a53ab22685c2a0b993ad2edb
abdsul <abdsul3434@gmail.com>
2020-10-20 16:58:29 +0400

    Отображение сообщений, добавление сообщений, фильтрация по тегу, хранение в бд

---
+++ src/main/java/com/abdsul/sweeter/SweeterApp.java
@@ -0,0 +1,11 @@
+package com.abdsul.sweeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweeterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SweeterApp.class, args);
+    }
+}
---
+++ src/main/java/com/abdsul/sweeter/controller/MainController.java
@@ -0,0 +1,62 @@
+package com.abdsul.sweeter.controller;
+
+import com.abdsul.sweeter.entity.Message;
+import com.abdsul.sweeter.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping(path = "/message")
+public class MainController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+
+    @GetMapping
+    public String main( Model model){
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Model model){
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Model model){
+
+
+       Iterable<Message> messages;
+       if(filter != null && !filter.isEmpty()) {
+           messages = messageRepo.findByTag(filter);
+       }else {
+           messages = messageRepo.findAll();
+       }
+
+
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
+
+}
