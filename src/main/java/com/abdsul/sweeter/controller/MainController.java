4d44299a6ab985d524247633a27d539e07a2c5c8
abdsul <abdsul3434@gmail.com>
2020-10-23 17:53:50 +0400

    Добавил сохранение и отображение авторов сообщений

--- src/main/java/com/abdsul/sweeter/controller/MainController.java
+++ src/main/java/com/abdsul/sweeter/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.abdsul.sweeter.controller;
 
 import com.abdsul.sweeter.entity.Message;
+import com.abdsul.sweeter.entity.User;
 import com.abdsul.sweeter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.MessageCodesResolver;
@@ -38,8 +40,11 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Model model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Model model){
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
