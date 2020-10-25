e3d2ca0034e0c4ef8ceeac4eb609325067d7cc69
abdsul <abdsul3434@gmail.com>
2020-10-25 19:02:00 +0400

    Email verification

--- src/main/java/com/abdsul/sweeter/service/UserService.java
+++ src/main/java/com/abdsul/sweeter/service/UserService.java
@@ -1,24 +1,74 @@
 package com.abdsul.sweeter.service;
 
+import com.abdsul.sweeter.entity.Role;
+import com.abdsul.sweeter.entity.User;
 import com.abdsul.sweeter.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private   UserRepo userRepo;
 
+    @Autowired
+    private MailSender mailSender;
+
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if(userFromDb != null){
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+
+        if(!StringUtils.isEmpty(user.getEmail())){
+            String message = String.format(
+                    "Hello? %s!" +
+                            "Welcome to sweeter- Twitter analog! Please, visit http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+
+
+        return true;
+
     }
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if(user == null){
+            return  false;
+        }
+
+        user.setActivationCode(null);
+
+        userRepo.save(user);
+
+        return true;
     }
 }
