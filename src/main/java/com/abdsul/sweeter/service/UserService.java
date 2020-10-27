a8af0a61e01af07de9bbde0c56a99835b257df83
abdsul <abdsul3434@gmail.com>
2020-10-27 23:45:29 +0400

    Начал шифрование паролей. Прервался на переход на Postgres

--- src/main/java/com/abdsul/sweeter/service/UserService.java
+++ src/main/java/com/abdsul/sweeter/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -21,6 +22,9 @@ public class UserService implements UserDetailsService {
     @Autowired
     private MailSender mailSender;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
@@ -33,6 +37,8 @@ public class UserService implements UserDetailsService {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         userRepo.save(user);
 
         sendMessage(user);
