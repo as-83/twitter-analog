45c76a390eded07a174ab7021969ab3f10824c17
abdsul <abdsul3434@gmail.com>
2020-11-29 17:30:42 +0400

    Turbolinks: ускорил отображение страниц

--- src/main/java/com/abdsul/sweeter/config/MvcConfig.java
+++ src/main/java/com/abdsul/sweeter/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.abdsul.sweeter.config;
 
+import com.abdsul.sweeter.util.RedirectInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,5 +30,10 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:////" + uploadPath + "/" );
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectInterceptor());
     }
 }
